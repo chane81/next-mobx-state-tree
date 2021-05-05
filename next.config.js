971f0b0bab0ed8ac9beb3c826c8b399bf5da3b61
chane81 <chane81@naver.com>
2021-05-05 16:32:50 +0900

    webpack 5 사용으로 수정

--- next.config.js
+++ next.config.js
@@ -7,6 +7,10 @@ const nextConfig = {
   // .env 값 세팅
   env: dotenv.config(`./env/.env.${isDev ? 'dev' : 'prod'}`).parsed || {},
 
+  future: {
+    webpack5: true,
+  },
+
   // 웹팩 설정
   webpack: (config, options) => {
     const originalEntry = config.entry;
@@ -17,11 +21,23 @@ const nextConfig = {
     //   config.devtool = 'inline-source-map';
     // }
 
+    // webpack 4 일 경우
     // dotenv 에 대한 'Module not found: Can't resolve 'fs' 에러 방지
+    // if (!options.isServer) {
+    //   config.node = {
+    //     fs: 'empty',
+    //   };
+    // }
+
+    // webpack 5 일 경우
+    // webpack 5 부터는 config.node 안씀 -> resolve.fallback 써야함
     if (!options.isServer) {
-      config.node = {
-        fs: 'empty',
-      };
+      // config.resolve.fallback = {
+      //   fs: false,
+      //   crypto: false,
+      //   path: false,
+      //   stream: false,
+      // };
     }
 
     // 개발모드인지 여부 true/false
