4d9b8ec8b1445b6e81cc8cc9dcc83979d327b0ac
chane81 <chane81@naver.com>
2021-06-22 02:21:24 +0900

    서버에서 store 값 전달시 주의사항 및 주석 추가

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,24 @@
 import { observer } from 'mobx-react-lite';
-import { useStore } from '../stores';
+import { initializeStore, IStore, useStore } from '../stores';
+import { GetServerSideProps } from 'next';
+import { getSnapshot } from 'mobx-state-tree';
 import env from '../../env';
 
-const Home = () => {
-  const store = useStore();
+const Home = ({ initialState }) => {
+  /**
+   * 아래와 같이 did not match 서버 <-> 클라이언트 warning 메시지가 나올 수 있는데
+   * useStore 사용시 서버에서 전달한 initialState 를 인자값으로 전달하여 warning 를 지울 수 있다.
+   * Warning: Text content did not match. Server: "0" Client: "3"
+   */
+  const store = useStore(initialState);
   const { fooModel, barModel } = store;
 
   const handleFooClick = () => {
-    console.log('env', env.MY_VAL);
-    fooModel.setCount();
+    fooModel.addCount();
   };
 
   const handleBarClick = () => {
-    barModel.setCount();
+    barModel.addCount();
   };
 
   console.log('rendering');
@@ -34,10 +40,19 @@ const Home = () => {
 
 export default observer(Home);
 
-export const getServerSideProps = () => {
+export const getServerSideProps: GetServerSideProps = async () => {
   console.log('server side: ', env.MY_VAL);
 
+  const store = initializeStore();
+  store.fooModel.setCount(3);
+
+  const initialState = getSnapshot<IStore>(store);
+
+  console.log('initialState', initialState);
+
   return {
-    props: {}
+    props: {
+      initialState
+    }
   };
 };
